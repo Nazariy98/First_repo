634c4fcab4eee6ee81201d1f8f1694c207c543af
Nazariy98 <166539816+Nazariy98@users.noreply.github.com>
2024-04-21 22:34:44 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-print("Hello World")
+from datetime import datetime
 
-print("Hello Git")
+def het_days_from_today(date):
+    dr_day = datetime.strptime(date, "%Y.%m.%d").date()
+    today = datetime.today().date()
+    res = (today - dr_day).days
+    return res
+print(het_days_from_today(date='2020.10.09'))
 
-print("Hi")
+# 1 випадок
+from random import randint, sample
 
-print("Hello Henri")
+def get_numbers_ticket(min, max, quantity):
+    lotery_list = set()
+    while len(lotery_list) != quantity:
+        lotery_list.add(randint(min, max))
+    return lotery_list
+lotery_list = get_numbers_ticket(1, 1000, 6)
+print(sorted(list(lotery_list)))
 
-print("Summer")
+# 2 випадок
+from random import randint, sample
+
+def get_numbers_ticket(min, max, quantity):
+    return sample(range(min, max+1), quantity)
+
+print(sorted(list(lotery_list)))
